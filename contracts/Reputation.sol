85fe9adf3c98c056a45adde38090f71c9932a87b
Laurence <laurence@extropy.io>
2016-07-17 06:06:28 +0100

    adding reputation

--- contracts/Reputation.sol
+++ contracts/Reputation.sol
@@ -1,3 +1,13 @@
-contract Reputation{
-  
+import "Owned.sol";
+import "UsersDB.sol";
+
+contract Reputation is Owned {
+
+mapping (address=>UsersDB.User) reputations;
+
+  function Reputation(){
+      owned();
+  }
+
+
 }
